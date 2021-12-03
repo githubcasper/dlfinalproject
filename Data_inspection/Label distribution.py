251d201786c191454ec43c20a1bcea0ee1c41b06
githubcasper <casper_haurum@hotmail.com>
2021-12-03 11:08:01 +0100

    Add Dataloaders

--- Data_inspection/Label distribution.py
+++ Data_inspection/Label distribution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
-df = pd.read_json('../News_Category_Dataset_v2.json', lines = True)
+df = pd.read_json('../Data/News_Category_Dataset_v2.json', lines=True)
 
 label_occurence = Counter(df['category']).most_common()
 plt.rcParams["figure.figsize"] = (18.5, 10.5)
--- Source/Dataloader.py
+++ Source/Dataloader.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, SubsetRandomSampler
+import torch
 
 
 class NewsDataset(Dataset):
@@ -14,3 +16,45 @@ class NewsDataset(Dataset):
         data_point_category = data_row[0]
         data_point_headline = data_row[1]
         return data_point_category, data_point_headline
+
+
+def get_loaders(batch_size: int, test_split: float, val_split: float, shuffle_dataset: bool, random_seed: int):
+    batch_size = batch_size
+    test_split = test_split
+    val_split = val_split
+    shuffle_dataset = shuffle_dataset
+    random_seed = random_seed
+
+    df = pd.read_json('../Data/News_Category_Dataset_v2.json', lines=True)
+
+    amount_of_data = len(df)
+    # Create indices to randomly split data into training and test sets:
+
+    dataset = NewsDataset('../Data/News_Category_Dataset_v2.json')
+
+    indices = list(range(amount_of_data))
+    split = int(np.floor(test_split * amount_of_data))
+    split_val = int(np.floor((val_split + test_split) * amount_of_data))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices, test_indices = indices[split_val:], indices[split:split_val], indices[:split]
+
+    # Create samplers and DataLoaders
+    test_sampler = SubsetRandomSampler(test_indices)
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+
+    test_loader = torch.utils.data.DataLoader(dataset,
+                                              batch_size=batch_size,
+                                              sampler=test_sampler)
+
+    train_loader = torch.utils.data.DataLoader(dataset,
+                                               batch_size=batch_size,
+                                               sampler=train_sampler)
+
+    val_loader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=batch_size,
+                                             sampler=val_sampler)
+
+    return train_loader, val_loader, test_loader
