0d38fd8b36c1604cbfc9931775378d0e03fdd49e
githubcasper <casper_haurum@hotmail.com>
2021-12-10 16:11:48 +0100

    Ready for hyperopt

--- Source/Call_Hyperopt.py
+++ Source/Call_Hyperopt.py
@@ -4,10 +4,10 @@ from collections import OrderedDict
 
 def get_best_hyper(objective):
     space = OrderedDict([('num_hidden_layers', hp.randint('num_hidden_layers', 1, 5)),
-                        ('size_hidden_layer', hp.randint('size_hidden_layer', 10, 30)),
-                        ('size_embed', hp.randint('size_embed', 10, 30)),
+                        ('size_hidden_layer', hp.randint('size_hidden_layer', 20, 40)),
+                        ('size_embed', hp.randint('size_embed', 30, 60)),
                         ('dropout', hp.uniform('dropout', 0, 0.5)),
-                        ('batch_size', hp.randint('batch_size', 50, 150))])
+                        ('batch_size', hp.randint('batch_size', 300, 400))])
 
     best = fmin(objective, space, algo=tpe.suggest, max_evals=10)
 
--- Source/hyperoptimization.py
+++ Source/hyperoptimization.py
@@ -6,8 +6,20 @@ from Dataloader import get_loaders
 from torchtext.data.utils import get_tokenizer
 import torch.nn.functional as F
 
+tokenizer = get_tokenizer('basic_english')
+vocab_sizes = VocabSizes(tokenizer)
+vocab_size, vocab_text = vocab_sizes.get_vocab_size_text()
+vocab_label = vocab_sizes.get_label_dict()
+vocab_int_to_label = vocab_sizes.get_int_to_label_dict()
+max_length = vocab_sizes.get_max_len()
+print(max_length)
+text_pipeline = lambda x: vocab_text(tokenizer(x))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def best_hyper(set_of_hyper):
+    print(set_of_hyper)
     num_hidden_layers = int(set_of_hyper['num_hidden_layers'])
     size_hidden_layer = int(set_of_hyper['size_hidden_layer'])
     emsize = int(set_of_hyper['size_embed'])
@@ -20,16 +32,7 @@ def best_hyper(set_of_hyper):
                                                         shuffle_dataset=True,
                                                         random_seed=123)
 
-    tokenizer = get_tokenizer('basic_english')
-    vocab_sizes = VocabSizes(tokenizer)
-    vocab_size, vocab_text = vocab_sizes.get_vocab_size_text()
-    vocab_label = vocab_sizes.get_label_dict()
-    vocab_int_to_label = vocab_sizes.get_int_to_label_dict()
-    max_length = vocab_sizes.get_max_len()
-    print(max_length)
-    text_pipeline = lambda x: vocab_text(tokenizer(x))
-
-    model = LSTMModel(vocab_size, emsize, dropout, num_hidden_layers, size_hidden_layer, max_length)
+    model = LSTMModel(vocab_size, emsize, dropout, num_hidden_layers, size_hidden_layer, max_length).to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -39,16 +42,19 @@ def best_hyper(set_of_hyper):
 
         for idx, (label, text) in enumerate(train_loader):
             optimizer.zero_grad()
+            loss = torch.autograd.Variable(torch.tensor(0, dtype=torch.float32, device=device))
             for i in range(len(text)):
                 input_list = text_pipeline(text[i])
                 while len(input_list) < max_length:
                     input_list.append(text_pipeline('<pad>')[0])
-                input_tensor = torch.tensor(input_list, dtype=torch.int64)
+                input_tensor = torch.tensor(input_list, dtype=torch.int64, device=device)
                 predicted_label = model(input_tensor)
-                one_hot_label = F.one_hot(torch.tensor(vocab_label[label[i]],
-                                                       dtype=torch.int64), num_classes=40).unsqueeze(0)
-                loss = criterion(predicted_label, one_hot_label.type(torch.FloatTensor))
-                loss.backward()
+
+                target_label = torch.tensor(vocab_label[label[i]], dtype=torch.int64, device=device).unsqueeze(0)
+
+                loss += criterion(predicted_label, target_label)
+
+            loss.backward()
             optimizer.step()
 
     def evaluate(val_loader):
@@ -58,13 +64,15 @@ def best_hyper(set_of_hyper):
             for idx, (label, text) in enumerate(val_loader):
                 loss = 0
                 for i in range(len(text)):
-                    predicted_label = model(torch.tensor(text_pipeline(text[i]), dtype=torch.int64))
-                    one_hot_label = F.one_hot(torch.tensor(vocab_label[label[i]], dtype=torch.int64), num_classes=40).unsqueeze(0)
-                    loss += criterion(predicted_label, one_hot_label.type(torch.FloatTensor))
+                    predicted_label = model(torch.tensor(text_pipeline(text[i]), dtype=torch.int64, device=device))
+                    target_label = torch.tensor(vocab_label[label[i]], dtype=torch.int64, device=device).unsqueeze(0)
+
+                    loss += criterion(predicted_label, target_label)
         return loss
 
-    epochs = 10
+    epochs = 5
     for epoch in range(epochs):
+        print(epoch)
         train(iter(train_loader), model)
 
     val_loss = evaluate(val_loader)
