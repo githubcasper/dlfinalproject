4e09aedfca96514604f510540053272f85a42b9b
andreastind <andreas@xdamgaard.dk>
2021-12-11 03:07:17 +0100

    Updated dictionary style in attempt to fix errors

--- Source/Call_Hyperopt.py
+++ Source/Call_Hyperopt.py
@@ -1,15 +1,29 @@
 from hyperopt import hp, fmin, tpe
-from collections import OrderedDict
-
+#from collections import OrderedDict
+#import hyperopt.pyll.stochastic
 
 def get_best_hyper(objective):
-    space = OrderedDict([('num_hidden_layers', hp.randint('num_hidden_layers', 1, 8)),
-                        ('size_hidden_layer', hp.randint('size_hidden_layer', 20, 60)),
-                        ('size_embed', hp.randint('size_embed', 40, 80)),
-                        ('dropout', hp.uniform('dropout', 0, 0.5)),
-                        ('batch_size', hp.randint('batch_size', 300, 500)),
-                        ('LR', hp.choice('LR', (0.01, 0.001)))])
+    space = {'num_hidden_layers': hp.randint('num_hidden_layers', 1, 8),
+             'size_hidden_layer': hp.randint('size_hidden_layer', 20, 60),
+             'size_embed':        hp.randint('size_embed', 40, 80),
+             'dropout':           hp.uniform('dropout', 0, 0.5),
+             'batch_size':        hp.randint('batch_size', 300, 500),
+             'LR':                hp.choice('LR', (0.01, 0.001))}
 
     best = fmin(objective, space, algo=tpe.suggest, max_evals=50)
 
     return best
+
+'''
+-------------------------------------------------------------------
+Det bør ikke gøre nogen forskel, men her er som det stod før:
+-------------------------------------------------------------------
+    
+space = OrderedDict([('num_hidden_layers', hp.randint('num_hidden_layers', 1, 8)),
+                    ('size_hidden_layer', hp.randint('size_hidden_layer', 20, 60)),
+                    ('size_embed', hp.randint('size_embed', 40, 80)),
+                    ('dropout', hp.uniform('dropout', 0, 0.5)),
+                    ('batch_size', hp.randint('batch_size', 300, 500)),
+                    ('LR', hp.choice('LR', (0.01, 0.001)))])
+
+'''
